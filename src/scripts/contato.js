fa9cb341fae2a54ea2445669710cabb20a5ec334
Rayane <rayanemoraes1705@hotmail.com>
2022-09-29 20:51:07 -0300

    atualizandoformulario

---
+++ src/scripts/contato.js
@@ -0,0 +1,9 @@
+function validarEmail(){
+  var email = document.querySelector('#email');
+  var error = document.querySelector('#error-email');
+  
+  if(!email.checkValidity()){
+    error.innerHTML = "Email invalido";  
+  }
+   
+}
